0dbaf0ef2354dfd90056399b930a48035a28eb16
Antoine Hars <antoine.hars@gmail.com>
2015-10-07 15:38:36 +0200

    comments in CreateDataFrameFromBabies.scala

--- src/main/scala/com/ahars/babies/CreateDataframeFromBabies.scala
+++ src/main/scala/com/ahars/babies/CreateDataframeFromBabies.scala
@@ -20,22 +20,26 @@ object CreateDataframeFromBabies {
       StructField("education", DataTypes.IntegerType)
     ))
 
+    // Configuration de Spark
     val conf = new SparkConf()
       .setAppName("create-dataframe-from-babies")
       .setMaster("local")
     val sc = new SparkContext(conf)
     val sqlc = new SQLContext(sc)
 
+    // Traitement du fichier babies23.txt
     val lines = sc.textFile("src/main/resources/babies23.txt")
       .map(line => line.dropWhile(_ == ' '))
       .filter(line => !line.startsWith("id"))
       .map(line => line.replaceAll("\\s+", ";"))
       .map(_.split(";"))
 
+    // Récupération des champs souhaités
     val rows = lines.map(fields => Row(fields(6).toInt, fields(4).toInt, fields(7).toInt, fields(9).toInt,
       fields(11).toInt, fields(12).toInt, fields(20).toInt, fields(10).toInt
     ))
 
+    // Création du Dataframe à partir des champs récupérés
     val df = sqlc.createDataFrame(rows, schema)
 
     df.show()
